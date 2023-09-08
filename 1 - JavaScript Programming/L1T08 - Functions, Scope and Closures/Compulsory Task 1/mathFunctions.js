417fd1401f33773ca228dfd882ec0806504c10a7
mark <markpackham1@gmail.com>
2023-09-08 08:03:19 +0100

    add subtractNumbers function

--- 1 - JavaScript Programming/L1T08 - Functions, Scope and Closures/Compulsory Task 1/mathFunctions.js
+++ 1 - JavaScript Programming/L1T08 - Functions, Scope and Closures/Compulsory Task 1/mathFunctions.js
@@ -1,5 +1,6 @@
 const numbers = [1, 2, 3];
 
+// findSum
 function findSum(numArray) {
   let sum = 0;
   for (let index = 0; index < numArray.length; index++) {
@@ -8,4 +9,13 @@ function findSum(numArray) {
   return sum;
 }
 
+// subtractNumbers
+function subtractNumbers(num1, num2) {
+  return num1 - num2;
+}
+
 console.log(`findSum returns a total of ${findSum(numbers)}`);
+
+console.log(
+  `subtractNumbers returns ${subtractNumbers(numbers[0], numbers[1])}`
+);
