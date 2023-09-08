da85f85d7199887b2c6648a0898ddc41b03abc3b
mark <markpackham1@gmail.com>
2023-09-08 08:18:02 +0100

    add divideNumbers function

--- 1 - JavaScript Programming/L1T08 - Functions, Scope and Closures/Compulsory Task 1/mathFunctions.js
+++ 1 - JavaScript Programming/L1T08 - Functions, Scope and Closures/Compulsory Task 1/mathFunctions.js
@@ -14,8 +14,35 @@ function subtractNumbers(num1, num2) {
   return num1 - num2;
 }
 
+// multiplyNumbers
+function multiplyNumbers(num1, num2) {
+  return num1 * num2;
+}
+
+// divideNumbers
+function divideNumbers(num1, num2) {
+  // make sure no division by zero is attempted
+  if (num2 === 0) {
+    return "YOU CANNOT DIVIDE BY ZERO!!!";
+  } else {
+    return num1 / num2;
+  }
+}
+
 console.log(`findSum returns a total of ${findSum(numbers)}`);
 
 console.log(
   `subtractNumbers returns ${subtractNumbers(numbers[0], numbers[1])}`
+);
+
+console.log(
+  `multiplyNumbers returns ${multiplyNumbers(numbers[2], numbers[0])}`
+);
+
+// console.log(
+//   `divideNumbers when dividing by ZERO returns ${divideNumbers(9999, 0)}`
+// );
+
+console.log(
+  `divideNumbers returns ${divideNumbers(findSum(numbers), numbers[2])}`
 );
