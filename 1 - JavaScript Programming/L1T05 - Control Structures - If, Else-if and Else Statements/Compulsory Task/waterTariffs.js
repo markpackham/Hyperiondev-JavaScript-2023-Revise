5be9cc2399c81d77edc4d56c912c283921da9658
mark <markpackham1@gmail.com>
2023-09-08 15:54:11 +0100

    use const more and variable names more

--- 1 - JavaScript Programming/L1T05 - Control Structures - If, Else-if and Else Statements/Compulsory Task/waterTariffs.js
+++ 1 - JavaScript Programming/L1T05 - Control Structures - If, Else-if and Else Statements/Compulsory Task/waterTariffs.js
@@ -1,6 +1,6 @@
-let litres = prompt("Enter the number of litres of water you have used: ");
+const litres = prompt("Enter the number of litres of water you have used: ");
 
-let kiloLitres = litres / 1000;
+const kiloLitres = litres / 1000;
 
 let answer = 0;
 
@@ -10,6 +10,7 @@ let answer = 0;
 // max amount of kilo litres a user can use
 const lowLevel = 6;
 const mediumLevel = 10.5;
+const lowAndMediumLevel = lowLevel + mediumLevel;
 const heavyLevel = 35;
 const superLevel = lowLevel + mediumLevel + heavyLevel;
 
@@ -18,12 +19,12 @@ const lowUser = 6 * 15.73;
 const mediumUser = 10.5 * 22.38;
 const heavyUser = 35 * 31.77;
 
-if (kiloLitres <= 6) {
+if (kiloLitres <= lowLevel) {
   answer = kiloLitres * 15.73;
-} else if (kiloLitres > 6 && kiloLitres <= 10.5) {
-  answer = lowUser + (kiloLitres - 6) * 22.38;
-} else if (kiloLitres > 10.5 && kiloLitres <= 35) {
-  answer = lowUser + mediumUser + (kiloLitres - 16.5) * 31.77;
+} else if (kiloLitres > lowLevel && kiloLitres <= mediumLevel) {
+  answer = lowUser + (kiloLitres - lowLevel) * 22.38;
+} else if (kiloLitres > mediumLevel && kiloLitres <= heavyLevel) {
+  answer = lowUser + mediumUser + (kiloLitres - lowAndMediumLevel) * 31.77;
 } else {
   // Else is used for anyone at super user level since that in theory has no limits
   answer = lowUser + mediumUser + heavyUser + (kiloLitres - superLevel) * 69.76;
