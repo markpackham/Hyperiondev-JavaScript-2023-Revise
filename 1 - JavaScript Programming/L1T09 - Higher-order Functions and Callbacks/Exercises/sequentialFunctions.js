4f7437d6ec085f8796ef4414e0a062f2ac97445a
mark <markpackham1@gmail.com>
2023-09-11 08:12:59 +0100

    demo sequential function

---
+++ 1 - JavaScript Programming/L1T09 - Higher-order Functions and Callbacks/Exercises/sequentialFunctions.js
@@ -0,0 +1,13 @@
+// Define two functions which execute sequentially
+function first() {
+  console.log(1);
+}
+function second() {
+  console.log(2);
+}
+// Call the functions
+first();
+second();
+// Output:
+// 1
+// 2
