ac6c32d5211ec1a0ab6c00b78d63f9b3ee8ac154
mark <markpackham1@gmail.com>
2023-09-11 09:53:34 +0100

    finish documentation

--- 1 - JavaScript Programming/L1T09 - Higher-order Functions and Callbacks/Compulsory Task 2/callBack.js
+++ 1 - JavaScript Programming/L1T09 - Higher-order Functions and Callbacks/Compulsory Task 2/callBack.js
@@ -6,21 +6,24 @@ let intervalID;
 let counter = 0;
 
 let startCounter = function () {
+  // Used the Mozilla Developer Network to understand setInterval structure
+  // https://developer.mozilla.org/en-US/docs/Web/API/setInterval
   intervalID = setInterval(
     // Create the logic needed in the setInterval() function for
-    //incrementing the counter and outputting to the console
+    // incrementing the counter and outputting to the console
 
     // an anonymous function
     function () {
       // my own safety measure to stop memory leak,
-      // counting to 100 should be enough for demo purposes
-      //  so we exited out of the function after that
-      if (counter > 100) {
-        return;
-      }
+      // counting to 200 should be enough for demo purposes
+      // so we exit out of the function after that
+      // you can't always count on a possibly distracted human to click the "Stop" button
+      // "return" is also simpler than just calling stopCounter() after 200 seconds
+      if (counter > 200) return;
+
       console.log(counter++);
     },
-    // increments occur ever 1 second
+    // increments occur ever 1 second / 1000ms
     1000
   );
 };
@@ -28,13 +31,19 @@ let startCounter = function () {
 function stopCounter() {
   // add the logic required to stop the counter
   // using clearInterval()
+
+  // Used the Mozilla Developer Network's reference to learn how to stop the counter from running
+  // https://developer.mozilla.org/en-US/docs/Web/API/clearInterval
+  // target the intervalID so we can stop the startCounter from running
+  clearInterval(intervalID);
 }
 
-// click test to help remind me about event listeners
+// click button test to help remind me about event listeners
 // function click_test() {
 //   console.log("Button has been clicked!");
 // }
 
+// target elements in html that users will be clicking on
 let startButton = document.getElementById("start");
 let stopButton = document.getElementById("stop");
 // let click_testButton = document.getElementById("click_test");
