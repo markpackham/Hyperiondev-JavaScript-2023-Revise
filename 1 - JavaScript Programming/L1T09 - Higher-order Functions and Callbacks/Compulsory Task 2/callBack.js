0474199fd9bbcac30b59b7b3444df8d41d6d358a
mark <markpackham1@gmail.com>
2023-09-11 09:23:53 +0100

    add click button test

--- 1 - JavaScript Programming/L1T09 - Higher-order Functions and Callbacks/Compulsory Task 2/callBack.js
+++ 1 - JavaScript Programming/L1T09 - Higher-order Functions and Callbacks/Compulsory Task 2/callBack.js
@@ -5,26 +5,45 @@
 let intervalID;
 let counter = 0;
 
-let startCounter = function() {
-    intervalID = setInterval(
-        // Create the logic needed in the setInterval() function for
-        //incrementing the counter and outputting to the console
-    );
-}
+let startCounter = function () {
+  console.log("Hello World");
+
+  intervalID = setInterval(
+    // Create the logic needed in the setInterval() function for
+    //incrementing the counter and outputting to the console
+
+    function () {
+      console.log(counter++);
+    },
+    1000
+  );
+};
 
 function stopCounter() {
-    // add the logic required to stop the counter
-    // using clearInterval()
+  // add the logic required to stop the counter
+  // using clearInterval()
 }
 
-let startButton = document.getElementById('start');
-let stopButton = document.getElementById('stop');
+// click test to help remind me about event listeners
+// function click_test() {
+//   console.log("Button has been clicked!");
+// }
+
+let startButton = document.getElementById("start");
+let stopButton = document.getElementById("stop");
+let click_testButton = document.getElementById("click_test");
 
 // The event listener below is missing a callback function argument;
 // this button should start the outputting of the counter to the console
-startButton.addEventListener("click", );
+
+// add my startCounter function
+startButton.addEventListener("click", startCounter);
 
 // The event listener below is missing a callback function argument;
-// this button should the counter from outputting to the console
-stopButton.addEventListener("click", );
+// this button should stop the counter from outputting to the console
 
+// add my stopCounter function
+stopButton.addEventListener("click", stopCounter);
+
+// add my click test to make sure the event listener is working
+// click_testButton.addEventListener("click", click_test);
