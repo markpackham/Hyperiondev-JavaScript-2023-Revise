9e77b7be49e93be187045271347d317b6cac7b4b
mark <markpackham1@gmail.com>
2023-09-12 07:38:58 +0100

    cite try catch block source

--- 1 - JavaScript Programming/L1T11 - Promises, Async and Await/Compulsory Task 2/catGifs.js
+++ 1 - JavaScript Programming/L1T11 - Promises, Async and Await/Compulsory Task 2/catGifs.js
@@ -1,11 +1,19 @@
 // Define an asynchronous function
-async function apiFunction() {
-  // Await to fetch data from the API
-  let item = await fetch(
-    "http://thecatapi.com/api/images/get?format=src&type=gif"
-  );
-  // Return results the console
-  console.log(item);
+async function getCatGif() {
+  // try / catch block learned from
+  // Pavlutin, D. (2023) How to use fetch with Async/await, Dmitri Pavlutin Blog.
+  // Available at: https://dmitripavlutin.com/javascript-fetch-async-await/
+  // (Accessed: 12 September 2023).
+  try {
+    // fetch cat from api
+    let cat = await fetch(
+      "http://thecatapi.com/api/images/get?format=src&type=gif"
+    );
+    //return cat
+    console.log(cat);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-apiFunction();
+getCatGif();
