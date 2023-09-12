8037406af8056ac2d81fd7fa8f0b42ea4f2e66bf
mark <markpackham1@gmail.com>
2023-09-12 06:57:28 +0100

    remove unneeded array

--- 1 - JavaScript Programming/L1T11 - Promises, Async and Await/Compulsory Task 1/pokemon.js
+++ 1 - JavaScript Programming/L1T11 - Promises, Async and Await/Compulsory Task 1/pokemon.js
@@ -1,10 +1,13 @@
-const squirtle = [];
-
-let pokemonPromise = new Promise(function (resolve, reject) {
+const pokemonPromise = new Promise(function (resolve, reject) {
+  // grab JSON file from site
   fetch("https://pokeapi.co/api/v2/pokemon/squirtle/")
-    // grab JSON data
+    // Used the Mozilla Developer Network to understand .json method
+    // https://developer.mozilla.org/en-US/docs/Web/API/Response/json
+    // parse JSON to JavaScript object
+    // res is short for "response"
     .then((res) => res.json())
 
+    // keep going through.then() process when there are no issues
     .then((result) => {
       items = result;
       // all goes well so resolve
@@ -17,10 +20,10 @@ let pokemonPromise = new Promise(function (resolve, reject) {
 });
 
 pokemonPromise.then(
-  // resolved outcome
+  // resolved outcome, dig through the result to get the details we want
   function (result) {
     console.log(
-      `Name: ${result.name} Weight: ${result.weight}\n Abilities: ${result.abilities[0].ability.name} and ${result.abilities[1].ability.name}`
+      `Name: ${result.name} - Weight: ${result.weight} - Abilities: ${result.abilities[0].ability.name} and ${result.abilities[1].ability.name}`
     );
   },
   // rejected outcome
