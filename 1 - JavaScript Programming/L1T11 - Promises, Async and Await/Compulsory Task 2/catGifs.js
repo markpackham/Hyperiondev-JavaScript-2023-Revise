2a4bea671dbfbc1c1646f3ef962cebf3005717a3
mark <markpackham1@gmail.com>
2023-09-12 09:43:31 +0100

    explain how I got beyond the cors issue

--- 1 - JavaScript Programming/L1T11 - Promises, Async and Await/Compulsory Task 2/catGifs.js
+++ 1 - JavaScript Programming/L1T11 - Promises, Async and Await/Compulsory Task 2/catGifs.js
@@ -4,7 +4,7 @@ const getCatGif = async () => {
   // Available at: https://dmitripavlutin.com/javascript-fetch-async-await/
   // (Accessed: 12 September 2023).
   try {
-    // fetch cat from api
+    // fetch cat gif from api
     let response = await fetch(
       // // Had to learn about CORS issues reading Stack Overflow
       // Stranger B.Stranger 9 et al. (1961)
@@ -12,18 +12,27 @@ const getCatGif = async () => {
       // Available at: https://stackoverflow.com/questions/29670703/how-to-use-cors-anywhere-to-reverse-proxy-and-add-cors-headers
       // (Accessed: 12 September 2023).
 
-      // Have to use CORS-Anyware to temporarily bypass CORS issues
-      // Rob--W (no date) PSA: Public demo server (cors-anywhere.herokuapp.com)
-      // will be very limited by January 2021, 31st · issue #301 · Rob--W/CORS-Anywhere, GitHub.
-      // Available at: https: github.com/Rob--W/cors-anywhere/issues/301 (Accessed: 12 September 2023).
+      // Attempted to use CORS Anywhere Proxy with no luck
+      // "https://cors-anywhere.herokuapp.com/http://thecatapi.com/api/images/get?format=src&type=gif"
+      // If I was using Express as my server I'd just use the cors node module, https://www.npmjs.com/package/cors
+      // But installing Express would have been overkill for this project
 
-      "https://cors-anywhere.herokuapp.com/http://thecatapi.com/api/images/get?format=src&type=gif"
+      // Had to install browser plugin to bypass CORS using vanilla Microsoft Edge
+      // (made sure it had many reviews & users given it's a security risk)
+      // Previously attempted using Brave Browser but I had too many security plugins & features
+      // https://chrome.google.com/webstore/detail/cors-unblock/lfhmikememgdcahcdlaciloancbhjino/
+      // Learned about plugin from
+      // Cors unblock - browser extension review (2020) YouTube.
+      // Available at: https://www.youtube.com/watch?v=8berLeTjKDM
+      // (Accessed: 12 September 2023).
+
+      "http://thecatapi.com/api/images/get?format=src&type=gif"
     );
 
     // check for 200 http status
     if (response.ok) {
       let catGif = response;
-      // output cat url
+      // output cat gif url
       console.log(catGif.url);
     } else {
       // show error if we don't get an ok
