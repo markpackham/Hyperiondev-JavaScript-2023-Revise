bd545d291d47cd16edc54cfb86440c588978964c
mark <markpackham1@gmail.com>
2023-09-12 08:02:10 +0100

    document cors issue

--- 1 - JavaScript Programming/L1T11 - Promises, Async and Await/Compulsory Task 2/catGifs.js
+++ 1 - JavaScript Programming/L1T11 - Promises, Async and Await/Compulsory Task 2/catGifs.js
@@ -1,4 +1,3 @@
-// Define an asynchronous function
 async function getCatGif() {
   // try / catch block learned from
   // Pavlutin, D. (2023) How to use fetch with Async/await, Dmitri Pavlutin Blog.
@@ -7,10 +6,23 @@ async function getCatGif() {
   try {
     // fetch cat from api
     let cat = await fetch(
-      "http://thecatapi.com/api/images/get?format=src&type=gif"
+      // // Had to learn about CORS issues reading Stack Overflow
+      // Stranger B.Stranger 9 et al. (1961)
+      // How to use CORS anywhere to reverse proxy and add cors headers, Stack Overflow.
+      // Available at: https://stackoverflow.com/questions/29670703/how-to-use-cors-anywhere-to-reverse-proxy-and-add-cors-headers
+      // (Accessed: 12 September 2023).
+
+      // Have to use CORS-Anyware to temporarily bypass CORS issues
+      // Rob--W (no date) PSA: Public demo server (cors-anywhere.herokuapp.com)
+      // will be very limited by January 2021, 31st · issue #301 · Rob--W/CORS-Anywhere, GitHub.
+      // Available at: https: github.com/Rob--W/cors-anywhere/issues/301 (Accessed: 12 September 2023).
+
+      "https://cors-anywhere.herokuapp.com/http://thecatapi.com/api/images/get?format=src&type=gif"
     );
-    //return cat
-    console.log(cat);
+
+    // output cat url
+    console.log(cat.url);
+    // catch and show error if something went wrong
   } catch (error) {
     console.log(error);
   }
