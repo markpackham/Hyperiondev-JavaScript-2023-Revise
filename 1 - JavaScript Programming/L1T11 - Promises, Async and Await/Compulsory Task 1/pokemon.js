815a48d91788144f0ea78e489c30a359cbdd7a07
mark <markpackham1@gmail.com>
2023-09-13 09:52:03 +0100

    simplify Promise code based on feedback
    Promises: Kindly note that when calling fetch("https://pokeapi.co/api/v2/pokemon/squirtle/"), a promise will be returned to indicate if the HTTP request was successful(resolved) or not(rejected). Therefore, you do not need to use "resolve" and "reject" because the promise will handle this either way based on the HTTP request. I therefore encourage you to simplify your code and to use the code structure similar to what is provided on page 6 in the task document. Simply, remove line 1 and replace lines 12 - 14 with lines 25 - 27. Thereafter remove the block of code from line 22 onwards. Based on the above changes, you can simplify the code to first fetch the data from the API, use the first .then() in the promise chain to parse the response as a json object (meaning to convert the response into a JSON object), then use the second .then() to help you to work with the JSON data. You can thereafter use the console.log function to return the specific information from the API. Lastly, implement error handling, you could use the .catch() method to catch any errors if the fetch fails

--- 1 - JavaScript Programming/L1T11 - Promises, Async and Await/Compulsory Task 1/pokemon.js
+++ 1 - JavaScript Programming/L1T11 - Promises, Async and Await/Compulsory Task 1/pokemon.js
@@ -1,33 +1,18 @@
-const pokemonPromise = new Promise(function (resolve, reject) {
-  // grab JSON file from site
-  fetch("https://pokeapi.co/api/v2/pokemon/squirtle/")
-    // Used the Mozilla Developer Network to understand .json method
-    // https://developer.mozilla.org/en-US/docs/Web/API/Response/json
-    // parse JSON to JavaScript object
-    // res is short for "response"
-    .then((res) => res.json())
+fetch("https://pokeapi.co/api/v2/pokemon/squirtle/")
+  // Used the Mozilla Developer Network to understand .json method
+  // https://developer.mozilla.org/en-US/docs/Web/API/Response/json
+  // parse JSON to JavaScript object
+  // res is short for "response"
+  .then((res) => res.json())
 
-    // keep going through.then() process when there are no issues
-    .then((result) => {
-      items = result;
-      // all goes well so resolve
-      resolve(items);
-    }),
-    // fire off an error if something went wrong
-    (error) => {
-      reject(error);
-    };
-});
-
-pokemonPromise.then(
-  // resolved outcome, dig through the result to get the details we want
-  function (result) {
-    console.log(
-      `Name: ${result.name} - Weight: ${result.weight} - Abilities: ${result.abilities[0].ability.name} and ${result.abilities[1].ability.name}`
-    );
-  },
-  // rejected outcome
-  function (error) {
+  .then(
+    // resolved outcome, dig through the result to get the details we want
+    function (result) {
+      console.log(
+        `Name: ${result.name} - Weight: ${result.weight} - Abilities: ${result.abilities[0].ability.name} and ${result.abilities[1].ability.name}`
+      );
+    }
+  )
+  .catch((error) => {
     console.log(error);
-  }
-);
+  });
