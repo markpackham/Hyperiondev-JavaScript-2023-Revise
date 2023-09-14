1a84e3bbdd48e23be4d4f2d5a881c3b082641d69
mark <markpackham1@gmail.com>
2023-09-14 10:24:03 +0100

    use only prompt boxes

--- 1 - JavaScript Programming/L1T13 - JSON and Client-Server Communication/Compulsory Task/disposableIncome.js
+++ 1 - JavaScript Programming/L1T13 - JSON and Client-Server Communication/Compulsory Task/disposableIncome.js
@@ -71,8 +71,15 @@ let electricity = new Expenses("electricity", 20, true);
 let fuel = new Expenses("fuel", 20, true);
 let wedding = new Expenses("wedding", 100, false);
 
+let incomeOutput = "";
 for (let index = 0; index < incomesArr.length; index++) {
-  console.log(
-    `${incomesArr[index].name} ${incomesArr[index].amount} ${incomesArr[index].recurring}`
-  );
+  incomeOutput +=
+    incomesArr[index].name +
+    "   £" +
+    incomesArr[index].amount +
+    "   recurring:" +
+    incomesArr[index].recurring +
+    "\n";
 }
+
+const incomePrompt = prompt(incomeOutput);
