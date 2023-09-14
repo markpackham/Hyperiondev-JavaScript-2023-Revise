afa1b02c3b455b91c7fbac537e5099568c0d0ffe
mark <markpackham1@gmail.com>
2023-09-14 10:03:40 +0100

    add new income function

--- 1 - JavaScript Programming/L1T13 - JSON and Client-Server Communication/Compulsory Task/disposableIncome.js
+++ 1 - JavaScript Programming/L1T13 - JSON and Client-Server Communication/Compulsory Task/disposableIncome.js
@@ -1,6 +1,34 @@
 const incomesArr = [];
 const expensesArr = [];
 
+function myLoad() {
+  let incomeList = document.getElementById("incomeList");
+  let expensesList = document.getElementById("expensesList");
+
+  if (sessionStorage.getItem("hasCodeRunBefore") === null) {
+    sessionStorage.setItem("incomes", JSON.stringify(incomesArr));
+    sessionStorage.setItem("hasCodeRunBefore", true);
+  } else {
+    //Get the array of person objects from sessionStorage and assign it to the array 'pers'
+    incomesArr = JSON.parse(sessionStorage.getItem("incomes"));
+
+    let i = 0;
+
+    //Loop through each person (p) in the pers array
+    /*For each person in the array create an option element that displays 
+      that person's name and add it to the select (dropdown) element on the HTML page */
+
+    incomesArr.forEach(function (inc) {
+      let optItem = document.createElement("option");
+      optItem.innerHTML = inco.name;
+      optItem.value = i;
+      i = i + 1;
+      htmlSelect.appendChild(optItem);
+    });
+  }
+}
+
+// Constructors for Income and Expenses
 function Income(name, amount, recurring) {
   this.name = name;
   this.amount = amount;
@@ -15,6 +43,18 @@ function Expenses(name, amount, recurring) {
   this.recurring = recurring;
 
   expensesArr.push(this);
+}
+
+// Add a new Income
+function addIncome() {
+  incomesArr = JSON.parse(sessionStorage.getItem("incomes"));
+  let newIncome = new Income(
+    document.getElementById("iName").value,
+    document.getElementById("amount").value,
+    document.getElementsByName("recurring").value
+  );
+  incomesArr.push(newIncome);
+  sessionStorage.setItem("incomes", JSON.stringify(incomesArr));
 }
 
 // Income
