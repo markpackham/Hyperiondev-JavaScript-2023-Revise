a69f67468c9f6b6671ec524eb2ed87978a77bff0
mark <markpackham1@gmail.com>
2023-09-14 11:52:30 +0100

    handle total expenses

--- 1 - JavaScript Programming/L1T13 - JSON and Client-Server Communication/Compulsory Task/disposableIncome.js
+++ 1 - JavaScript Programming/L1T13 - JSON and Client-Server Communication/Compulsory Task/disposableIncome.js
@@ -4,6 +4,9 @@ const expensesArr = [];
 let incomeOutput = "";
 let expenseOutput = "";
 
+let totalIncome = 0;
+let totalExpenses = 0;
+
 // Constructors for Income and Expenses
 function Income(name, amount, recurring) {
   this.name = name;
@@ -49,13 +52,13 @@ let wedding = new Expenses("wedding", 100, false);
 
 // Show income
 const displayIncome = () => {
-  for (let index = 0; index < incomesArr.length; index++) {
+  for (let i = 0; i < incomesArr.length; i++) {
     incomeOutput +=
-      incomesArr[index].name +
+      incomesArr[i].name +
       "   £" +
-      incomesArr[index].amount +
+      incomesArr[i].amount +
       "   recurring:" +
-      incomesArr[index].recurring +
+      incomesArr[i].recurring +
       "\n";
   }
 };
@@ -63,18 +66,38 @@ const displayIncome = () => {
 // Show expenses - very simular to income function did consider refactoring both into 1 function
 // and have it take arrays as a parameter but I want to keep the incomeOutput & expenseOutput seperate
 const displayExpenses = () => {
-  for (let index = 0; index < expensesArr.length; index++) {
+  for (let i = 0; i < expensesArr.length; i++) {
     expenseOutput +=
-      expensesArr[index].name +
+      expensesArr[i].name +
       "   £" +
-      expensesArr[index].amount +
+      expensesArr[i].amount +
       "   recurring:" +
-      expensesArr[index].recurring +
+      expensesArr[i].recurring +
       "\n";
   }
 };
+
+// Show Total Income
+let getTotalIncome = () => {
+  for (let i = 0; i < incomesArr.length; i++) {
+    totalIncome += incomesArr[i].amount;
+  }
+};
+
+let getTotalExpenses = () => {
+  for (let i = 0; i < expensesArr.length; i++) {
+    totalExpenses += expensesArr[i].amount;
+  }
+};
+
+getTotalIncome();
+getTotalExpenses();
 
 displayIncome();
 displayExpenses();
 console.log(incomeOutput);
 console.log(expenseOutput);
+
+console.log("Total income: £" + totalIncome);
+console.log("Total expenses: £" + totalExpenses);
+console.log("Disposable income: £" + Number(totalIncome - totalExpenses));
