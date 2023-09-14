3d2661b76c282541c6d1ec69feec588a91cc1104
mark <markpackham1@gmail.com>
2023-09-14 06:46:03 +0100

    add expense and income objects

---
+++ 1 - JavaScript Programming/L1T13 - JSON and Client-Server Communication/Compulsory Task/disposableIncome.js
@@ -0,0 +1,32 @@
+const incomesArr = [];
+const expensesArr = [];
+
+function Income(name, amount, recurring) {
+  this.name = name;
+  this.amount = amount;
+  this.recurring = recurring;
+
+  incomesArr.push(this);
+}
+
+function Expenses(name, amount, recurring) {
+  this.name = name;
+  this.amount = amount;
+  this.recurring = recurring;
+
+  expensesArr.push(this);
+}
+
+// Income
+let janitor = new Income("salary", 100, true);
+let cleaner = new Income("salary", 200, true);
+let bouncer = new Income("salary", 300, true);
+let sales = new Income("wage", 400, false);
+let waiter = new Income("tips", 10, false);
+
+// Expenses
+let food = new Expenses("food", 10, true);
+let water = new Expenses("water", 10, true);
+let electricity = new Expenses("electricity", 20, true);
+let fuel = new Expenses("fuel", 20, true);
+let wedding = new Expenses("wedding", 100, false);
