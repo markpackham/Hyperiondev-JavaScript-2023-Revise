2435fa5d527de6ca8cca7e4079c81276ebcff21b
mark <markpackham1@gmail.com>
2023-09-18 14:14:18 +0100

    get random meal in array position

--- 1 - JavaScript Programming/L1T14 - Capstone Project - Build Your Developer Portfolio/capstone_project/portfolio/take_order_app/take_order.js
+++ 1 - JavaScript Programming/L1T14 - Capstone Project - Build Your Developer Portfolio/capstone_project/portfolio/take_order_app/take_order.js
@@ -1,9 +1,11 @@
 const mainIngredientFilter =
   "https://www.themealdb.com/api/json/v1/1/filter.php?i=";
 
-let mainIngredient = prompt(
-  "What is the main ingredient you would like to use eg(chicken, beef, salmon, pork, avocado)?"
-);
+let mainIngredient = "avocado";
+
+// mainIngredient = prompt(
+//   "What is the main ingredient you would like to use eg(chicken, beef, salmon, pork, avocado)?"
+// );
 
 let mainIngredientAnswer = mainIngredientFilter + mainIngredient;
 
@@ -11,15 +13,25 @@ const fetchMainIngredientMeals = async () => {
   try {
     const response = await fetch(mainIngredientAnswer);
     const data = await response.json();
-    console.log(data);
+    return data;
   } catch (error) {
     console.error(error);
   }
 };
 
 let meal = fetchMainIngredientMeals();
-
 let mealsArr = [];
-// Math.random() learned from
-// MDN Web Docs https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-let randomMeal = Math.random(0, mealsArr.length - 1);
+
+// Promise gets resolved
+meal.then(function (result) {
+  // Obtain the array of the object
+  let mealsList = result.meals;
+
+  // add to new array to just hold meal names
+  const mealNames = mealsList.map((meal) => meal.strMeal);
+
+  // Math.random() learned from
+  // MDN Web Docs https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  randomMeal = Math.floor(Math.random() * (mealNames.length - 1));
+  console.log(mealNames[randomMeal]);
+});
